e72019a8d17e1dd25a1a452caade325f509a73c1
root <root@DESKTOP-OBMO4FT.>
2025-01-08 15:29:46 +0000

    refactor: rename Counter.sol to Station.sol and update tests

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,47 +1,53 @@
-// SPDX-License-Identifier: UNLICENSED
+//I am commennt
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {PoliceStation} from "../src/Counter.sol";
-
+import {PoliceStation} from "../src/Station.sol";
 
 contract CounterTest is Test {
     PoliceStation public counter;
 
-    function setUp() public {
-
-        counter = new PoliceStation();
-        console.log("Set up is being compleetd ");
-        
+    enum CaseCategory {
+        Theft,
+        Assault,
+        Fraud,
+        MissingPerson,
+        Narcotics
     }
 
+    function setUp() public {
+        counter = new PoliceStation();
+        console.log("Set up is being completed");
+    }
 
     function testAddOfficer() public {
-
-         address officer = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        address officer = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         PoliceStation.Post post = PoliceStation.Post.DGP;
         string memory name = "John Doe";
         uint256 salary = 100000;
         uint104 id = 1;
         uint256 age = 30;
+        PoliceStation.CaseCategory speciality = PoliceStation.CaseCategory.Fraud;
 
-
-       counter.addOfficer(
+        counter.addOfficer(
             officer,
             post,
             name,
             salary,
             id,
-            age
+            age,
+            speciality
         );
 
-                (
+        (
             address storedOfficer,
             PoliceStation.Post storedPost,
             string memory storedName,
             uint256 storedSalary,
             uint104 storedId,
-            uint256 storedAge
+            uint256 storedAge,
+            PoliceStation.CaseCategory storedspeciality
         ) = counter.officers(0);
 
         // Assertions
@@ -51,15 +57,6 @@ contract CounterTest is Test {
         assertEq(storedSalary, salary);
         assertEq(storedId, id);
         assertEq(storedAge, age);
+        assertEq(uint(storedspeciality), uint(speciality));
     }
-
-
-
-
-
-
-
-    }
-
-   
-
+}
